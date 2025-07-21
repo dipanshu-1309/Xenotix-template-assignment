ad660b1c7020f72e9f4a53a39828ef999e60310b
Dipanshu Rawat <dipanshurawat4@gmail.com>
2025-07-21 16:06:16 +0530

    Started Making Page Responsive

--- src/app/page.js
+++ src/app/page.js
@@ -21,51 +21,74 @@ import Group157 from "./components/Group157"
 export default function Home() {
   return (
     <div className="maindiv">
-      <div className="ellipse-6"></div>
-      <Frame32 /> 
-      <Frame31 />
-      <div className="Text1">Xenotix Tech</div>
-      <div className="Text2">Got a startup Idea !</div>
-      <div className="Text3">Let&apos;s Turn It Into Reality.</div>
+      <div className="div-1">
+        <div className="ellipse-6"></div>
+        <div className="background-effect"></div>
+        <Group158 />
+        <Group157 />
+        <Frame32 /> 
+        <Frame31 />
+        <img src="/assets/images/decoration.png" alt="image0" className="image0" />
+        <Vectortwo />
+        <Vectorthree />
+     
+      </div>
 
-      <img src="/assets/images/decoration.png" alt="image0" className="image0" />
-      
-      <Vector />
-      <Vectortwo />
-      <Vectorthree />
-      <Frame10/>
-
-      <div className="middle-block-1">
+      <div className="div-2">
+        <div className="Text1">Xenotix Tech</div>
+        <div className="Text2">Got a startup Idea !</div>
+        <div className="Text3">Let&apos;s Turn It Into Reality.</div>
+        <Vector />
+        <div className="middle-block-1">
         <img src="/assets/images/2944870.png" alt="image1" className="image1" />
-      </div>
-      <div className="middle-block-2">
-        <img src="/assets/images/aws.png" alt="image2" className="image2" />
-      </div>
-      <div className="middle-block-3">
-        <img src="/assets/images/N.png" alt="image3" className="image3" />
-      </div>
-      <div className="middle-block-4">
-        <img src="/assets/images/SEO.png" alt="image4" className="image4" />
-      </div>
-      <div className="middle-block-5">
-        <img src="/assets/images/NFT.png" alt="image5" className="image5" />
-      </div>
-      <div className="middle-block-6">
-        <img src="/assets/images/GPT.png" alt="image6" className="image6" />
+        </div>
+        <div className="middle-block-2">
+          <img src="/assets/images/aws.png" alt="image2" className="image2" />
+        </div>
+        <div className="middle-block-3">
+          <img src="/assets/images/N.png" alt="image3" className="image3" />
+        </div>
+        <div className="middle-block-4">
+          <img src="/assets/images/SEO.png" alt="image4" className="image4" />
+       </div>
+       <div className="middle-block-5">
+          <img src="/assets/images/NFT.png" alt="image5" className="image5" />
+        </div>
+        <div className="middle-block-6">
+          <img src="/assets/images/GPT.png" alt="image6" className="image6" />
+        </div>
       </div>
 
-      <div className="background-effect"></div>
+      <div className="div-3">
+        <Uiux />
+        <Frame29 />
+        <Frame28 />
+        <Frame30 />
+      </div>
 
-      <Group158 />
-      <Frame43 />
-      <Frame43A />
-      <MetaAds /> 
-      <Frame15 /> 
-      <Uiux />
-      <Frame29 />
-      <Frame28 />
-      <Frame30 />
-      <Group157 /> 
+      <div className="div-4">
+       <Frame10/>
+      </div>
+      <div className="div-5">
+       <Frame43 />
+       <Frame43A />
+       <MetaAds /> 
+       <Frame15 /> 
+      </div>
+      
+     
+      
+
+      
+
+      
+
+    
+
+      
+      
+      
+     
 
     </div>
   )
